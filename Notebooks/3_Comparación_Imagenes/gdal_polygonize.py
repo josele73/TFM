6fbeebb4af5fa011b55d7a34e1a8d9cbcba368b8
josele73 <44363614+josele73@users.noreply.github.com>
2020-05-21 16:29:41 +0200

    Delete gdal_polygonize.py

--- Notebooks/3_Comparación_Imagenes/gdal_polygonize.py
+++
@@ -1,4 +0,0 @@
-#!C:/Users/josel/Anaconda3\python.exe
-# EASY-INSTALL-SCRIPT: 'GDAL==2.3.3','gdal_polygonize.py'
-__requires__ = 'GDAL==2.3.3'
-__import__('pkg_resources').run_script('GDAL==2.3.3', 'gdal_polygonize.py')
